b367a8ffbf74ea3fad5431345a055b3c746292f7
zabdiel reyes <zabdielreyes@hotmail.com>
2024-10-08 18:02:04 -0400

    for Iteración (instrucción) Conceptos básicos de C# para principiantes

--- for Iteración/Iteracion.cs
+++ for Iteración/Iteracion.cs
@@ -4,4 +4,26 @@ using System;Linq;
 using System.text;
 using System.threading.Task;
 
+namespace Iteraion
+{
+    static void Main(string[] args)
+        
+        for (int i = 0; i< 10; i++)
+        {
+        //console.writeLine(i);
+        if (int i == 7)
+        {
+            Console.WriteLine("Found Seven");
+            break;
+        }
 
+            
+        }
+    }
+
+    for (int myValue = 0; myValue <12; myValue++)
+    {
+    Console.WriteLine(myValue);
+    }
+    Console.ReadLine();
+}
