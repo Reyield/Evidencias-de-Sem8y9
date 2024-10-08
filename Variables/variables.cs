c075a1e51a96563406fc76864715d54c99061685
zabdiel reyes <zabdielreyes@hotmail.com>
2024-10-08 17:44:31 -0400

    a

--- Variables/variables.cs
+++ Variables/variables.cs
@@ -4,11 +4,11 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            int x;
-            int y;
+            //int x;
+            //int y;
 
-            x = 7;
-            y = x + 3;
+            //x = 7;
+            //y = x + 3;
 
             Console.WriteLine(y);
             Console.WriteLine();
---
+++ for Iteración/Iteracion.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.collection.generic;
+using System;Linq;
+using System.text;
+using System.threading.Task;
+
+
